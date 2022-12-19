9943dc53b2417bab96a35a0a2faf378af59058de
kch1-jpeg <116844422+kch1-jpeg@users.noreply.github.com>
2022-12-19 19:56:02 +0000

    add weather css

--- src/Weather.js
+++ src/Weather.js
@@ -4,40 +4,49 @@ import "./Weather.css";
 export default function Weather() {
   return (
     <div className="Weather">
-      <form>
-        <div class="row">
-          <div class="col-9">
-            <input
-              type="search"
-              placeholder="Type a city..."
-              className="form-control"
-              autocomplete="off"
-            />
+      <div className="weather-app-wrapper">
+        <div className="weather-app">
+          <form className="mb-3">
+            <div className="row">
+              <div className="col-9">
+                <input
+                  type="search"
+                  placeholder="Type a city..."
+                  className="form-control"
+                  autocomplete="off"
+                />
+              </div>
+              <div className="col-3">
+                <input type="submit" value="Search" className="search-button" />
+              </div>
+            </div>
+          </form>
+          <div className="overview">
+            <h1 className="mb-4">Lisbon</h1>
+            <ul>
+              <li className="current-time">Wednesday 7:00</li>
+              <li>Last Updated:</li>
+              <li> Mostly Cloud</li>
+            </ul>
           </div>
-          <div class="col-3">
-            <input type="submit" value="Search" className="search-button" />
+          <div className="row">
+            <div className="col-6">
+              <div className="weather-temperature">
+                <img
+                  src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+                  alt="Mostly cloudy"
+                />
+                6 °C
+              </div>
+            </div>
+            <div className="col-6">
+              <ul className="desc">
+                <li>Precipitation: 15%</li>
+                <li>Humidity: 72%</li>
+                <li>Wind: 13 km/h</li>
+              </ul>
+            </div>
           </div>
-        </div>
-      </form>
-      <h1>Lisbon</h1>
-      <ul>
-        <li> Wednesday 7:00</li>
-        <li> Mostly Cloud</li>
-      </ul>
-      <div className="row">
-        <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-            alt="Mostly cloudy"
-          />
-          6 °C
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Precipitation: 15%</li>
-            <li>Humidity: 72%</li>
-            <li>Wind: 13 km/h</li>
-          </ul>
         </div>
       </div>
     </div>
